509ec7f864c781e4a619c0a3253d2b4a8750148e
sallop <shiogai1987@yahoo.co.jp>
2015-10-01 01:03:30 +0900

    new file:   CatWings.js

---
+++ chapter6/CatWings.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+function Cat() {
+	this.legs = 4;
+	this.say = function(){
+		return "meaowww";
+	}
+}
+
+function Bird() {
+	this.wings = 2;
+	this.fly = true;
+}
+
+function CatWings() {
+	Cat.apply(this);
+	Bird.apply(this);
+}
+
+var jane = new CatWings();
+console.dir( jane );
+console.log( jane.say() );
